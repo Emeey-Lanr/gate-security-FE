1413bc8009dbdc294b012c2bcfb0863a863664ee
Emmanuel Oyelowo <emeeylanr04@gmail.com>
2026-03-31 16:42:07 +0100

    fix:removed unused value

--- src/layouts/AdminLayouts.tsx
+++ src/layouts/AdminLayouts.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "../features/admin/components/AdminSidebar"
-import { useState } from "react"
+
 import AdminHeader from "../features/admin/components/AdminHeader"
 
 const AdminLayouts = () => {
-  const [hide, _] = useState<boolean>(false)
+
   return (
     <div className="lg:min-h-screen lg:w-full lg:flex">
         <Sidebar/>
